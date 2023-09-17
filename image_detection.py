732a0b6815f113eb10c8c98d537e4ea884660444
aksharpat <akshar0308@gmail.com>
2023-09-16 19:08:27 -0500

    wire finder

--- image_detection.py
+++ image_detection.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Load the image
-image = cv2.imread("thomp2.jpg")
+image = cv2.imread("thomp4.jpg")
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
